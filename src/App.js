3509642f34614926240ae758ceaa01a3d9011d40
Eunice Dhivya <divi.seed@gmail.com>
2022-09-17 23:53:48 +0530

    add file upload feature with parsing csv function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+// import * as d3 from 'd3'; 
+import { csvParse } from 'd3'; 
+import { useState } from 'react';
 
 function App() {
+  const [ csvData, setCsvData ] = useState([]);
+  const [ error, setError ] = useState("");
+
+  const onFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      var reader = new FileReader();
+      reader.readAsText(file, "UTF-8");
+      reader.onload = function (evnt) {
+          const parsedData = csvParse(evnt.target.result);
+          setCsvData(parsedData);
+      }
+      reader.onerror = function (evnt) {
+          setError("file loading error")
+      }
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="fileupload">
+        <input type="file" id="myfile" name="file_upload" onChange={onFileChange} />
+        {error && error}
       </header>
+      <div className='scrollbox'>
+        {csvData.length === 0 ? null : JSON.stringify(csvData) }
+      </div>
     </div>
   );
 }
