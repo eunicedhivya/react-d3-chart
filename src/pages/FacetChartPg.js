62fda26ed5e512e70b28784e1fe97e828468d67d
Eunice Dhivya <divi.seed@gmail.com>
2022-09-18 19:37:45 +0530

    add pages for facet chart and home

--- src/pages/FacetChartPg.js
+++ src/pages/FacetChartPg.js
@@ -3,6 +3,7 @@ function FacetChartPg() {
   return (
     <div className="container page facetchartpg">
       <h2> Facet Chart Demo</h2>
+      <p> Sample dataset with 3 columns and 1000 rows data subplots depend on the no of columns in dataset</p>
       <FacetChart />
     </div>
   )
--- src/pages/HomePg.js
+++ src/pages/HomePg.js
@@ -4,8 +4,8 @@ function HomePg() {
   return (
     <div className="container page homepg">
         <h1>Chart generator on file upload</h1>
-        <Link to="/demo"> Demo </Link>
-        <Link to="/facet-chart"> Facet Chart Demo </Link>
+        <Link className="btn" to="/demo"> Demo </Link>
+        <Link className="btn" to="/facet-chart"> Facet Chart Demo </Link>
 
 
     </div>
